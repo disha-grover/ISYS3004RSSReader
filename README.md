c2b35e9f9d832982a2fbbe8e0c48566552f76693
Disha Grover <101254334+disha-grover@users.noreply.github.com>
2023-04-03 16:14:16 +0800

    edited JS and moved files to their relevant folders

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ISYS3004RSSReader
+Repository for the unit ISYS3004 - Mobile and Web Technologies. This repository includes the RSS Reader created in Week 5 Module. 
---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+function addRSStoDOM(data) {
+  let itemsContainer = document.createElement('DIV');
+  for (let i = 0, t = data.items.length; i < t; ++i) {
+    let item = data.items[i];  
+    let itemContainer = document.createElement('DIV');
+    let itemLinkElement = document.createElement('A');
+    itemLinkElement.setAttribute('href', item.link);
+    itemLinkElement.innerText = item.title;    
+    let itemTitleElement = document.createElement('H2');
+    itemTitleElement.appendChild(itemLinkElement);
+    let itemDescriptionElement = document.createElement('P');
+    itemDescriptionElement.innerHTML = item.description;
+    itemContainer.appendChild(itemTitleElement);
+    itemContainer.appendChild(itemDescriptionElement);
+    itemsContainer.appendChild(itemContainer);
+  }
+  let titleElement = document.createElement('H1');
+  titleElement.innerText = data.feed.title;
+  content.appendChild(titleElement);
+  content.appendChild(itemsContainer);
+}
+
+var content = document.getElementsByTageName('main')[0]
+
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function(){
+  if (xhr.status >=200 && xhr.status < 300) {
+    json = JSON.parse(xhr.responseText)
+    console.log(json)
+  } else{
+    console.log("The request failed")
+    content.innerHTML = "The request failed, please check your RSS URL."
+  }
+}
+
+
+let addFeedButton = document.getElementById("add-feed");
+let newRSSInput = document.getElementById("rss-input");
+
+function onAddRSSClicked(event) {
+  let URL = newRSSInput.value;
+  newRSSInput.value = "";
+  xhr.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=' + URL);
+  xhr.send();
+}
+addFeedButton.addEventListener('click', onAddRSSClicked);
